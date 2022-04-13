cfdd3a0403972bfe487544c72c80b441af9830b0
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-13 09:45:46 +0200

    feat: add & mock getWorkouts()

--- api/api.ts
+++ api/api.ts
@@ -3,15 +3,25 @@ import { IWorkout } from "../interfaces/IWorkout";
 
 interface IApi {
   sendWorkout(data: IWorkout): Promise<IStandardResponse>;
+  getWorkouts(userId: string): Promise<IWorkout[]>
 }
 
-class Api implements IApi {
-  async sendWorkout(data: IWorkout): Promise<IStandardResponse> {
-    const response = await fetch("/api/mail", {
+class Api {
+  static async sendWorkout(data: IWorkout): Promise<IStandardResponse> {
+    const response = await fetch("/api", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ data }),
     });
+    return await response.json();
+  }
+  static async getWorkouts(userId: string): Promise<IWorkout[]> {
+    const response = await fetch(
+      "/api?" +
+        new URLSearchParams({
+          userId: userId,
+        })
+    );
     return await response.json();
   }
 }
--- mocks/mockApi.ts
+++ mocks/mockApi.ts
@@ -1,8 +1,12 @@
 import { IWorkout } from "../interfaces/IWorkout";
-import {IStandardResponse} from "../interfaces/IApi";
+import { IStandardResponse } from "../interfaces/IApi";
+import { mockWorkoutList } from "../mocks/mockWorkoutList";
 
 export class MockApi {
   static async sendWorkout(data: IWorkout): Promise<IStandardResponse> {
-    return await {statusCode: 200, message: "message received"};
+    return await { statusCode: 200, message: "message received" };
+  }
+  static async getWorkouts(userId: string): Promise<IWorkout[]> {
+    return await mockWorkoutList.filter((workout) => workout.userId === userId);
   }
 }
