5b88f4f8c4aa06e03dbdcffeb51345b3b9e445ec
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-13 14:47:59 +0200

    feat: update workout notes; refactor: delete item

--- components/WorkoutListItem.tsx
+++ components/WorkoutListItem.tsx
@@ -1,19 +1,48 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+
 import { IWorkout } from "../interfaces/IWorkout";
 import { MockApi } from "../mocks/mockApi";
+import { WorkoutNotes } from "./WorkoutNotes";
 
 export const WorkoutListItem = ({ workout }: { workout: IWorkout }) => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [workoutData, setWorkoutData] = useState<IWorkout | null>(workout);
+  const [workoutData, setWorkoutData] = useState<IWorkout>();
+  const [editModeOn, setEditModeOn] = useState(false);
+  const [shouldDelete, setShouldDelete] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data: any) => {
+    setEditModeOn(false);
+    const tempWorkoutData = workoutData;
+    if (tempWorkoutData) {
+      tempWorkoutData.notes = data.notes;
+    }
+    setWorkoutData(tempWorkoutData);
+  };
+
+  useEffect(() => {
+    setWorkoutData(workout);
+  });
 
   const deleteWorkout = () => {
     if (workout.workoutId) {
       MockApi.deleteWorkout(workout.workoutId);
-      setWorkoutData(null);
+      setShouldDelete(true);
     }
   };
 
-  const handleClick = () => {
+  const updateWorkout = () => {
+    editModeOn ? setEditModeOn(false) : setEditModeOn(true);
+  };
+
+  const handleClick = (e: any) => {
+    e.preventDefault();
     isOpen ? setIsOpen(false) : setIsOpen(true);
   };
 
@@ -28,73 +57,106 @@ export const WorkoutListItem = ({ workout }: { workout: IWorkout }) => {
     return `${hrs} : ${mins} : ${secs}`;
   };
 
-  if (!workoutData) {
+  if (shouldDelete) {
     return null;
   }
 
   return (
     <>
-      <div className="bg-blue-100/90 border-t border-blue-900/20 p-3 flex justify-between">
-        <span>
-          <span className="px-2">{workout.date}</span>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="hidden"></input>
+        <div className="bg-blue-100/90 border-t border-blue-900/20 p-3 flex justify-between">
           <span>
-            {workout.parts.map((part, i) => {
-              return (
-                <>
-                  {i % 2 === 1 && <span>&ndash;</span>}
-                  <span className="uppercase text-sm px-2 tracking-wide">
-                    {part.discipline}
-                  </span>
-                </>
-              );
-            })}
-          </span>
-        </span>
-        <button onClick={handleClick} className="text-sm hover:font-semibold">
-          details
-        </button>
-        <span onClick={deleteWorkout} className="cursor-pointer">
-          <img src="/delete_icon.svg" className="ml-2 h-5 w-5" />
-        </span>
-      </div>
-      {isOpen && (
-        <div>
-          <table className="border-t  pb-2 table-auto w-full text-center">
-            <thead className="uppercase text-xs text-slate-700 bg-slate-50">
-              <th className="p-1 bg-slate-100/50">discipline</th>
-              <th className="bg-slate-100/50">distance</th>
-              <th className="bg-slate-100/50">duration</th>
-              <th colSpan={2} className="bg-slate-100/50">
-                &nbsp;
-              </th>
-            </thead>
-            <tbody className=" ">
-              {workout.parts.map((part) => {
+            <span className="px-2">{workoutData?.date}</span>
+            <span>
+              {workoutData?.parts.map((part, i) => {
                 return (
-                  <tr className="border-t">
-                    <td className="p-2 text-xs font-semibold">
-                      {part.discipline.toUpperCase()}
-                    </td>
-                    <td className="p-4 flex justify-center align-center">
-                      <span>{part.distanceInMeters / 1000} km</span>
-                    </td>
-                    <td className="p-2">{toHours(part.durationInSeconds)}</td>
-                  </tr>
+                  <span key={`part-${part.discipline}`}>
+                    {i % 2 === 1 && <span>&ndash;</span>}
+                    <span
+                      key={part.discipline}
+                      className="uppercase text-sm px-2 tracking-wide"
+                    >
+                      {part.discipline}
+                    </span>
+                  </span>
                 );
               })}
-            </tbody>
-            <tfoot className="border-t bg-stone-100/50">
-              <tr>
-                <td colSpan={3} className="px-10 py-2 text-left">
-                  <span className="text-slate-600 text-sm italic">
-                    {workout.notes}
-                  </span>
-                </td>
-              </tr>
-            </tfoot>
-          </table>
+            </span>
+          </span>
+          <span className="flex">
+            <button
+              onClick={(e) => handleClick(e)}
+              className="text-sm hover:font-semibold"
+            >
+              details
+            </button>
+            <span onClick={updateWorkout} className="cursor-pointer">
+              <img src="/edit_icon.svg" className="ml-2 h-5 w-5" />
+            </span>
+            <span onClick={deleteWorkout} className="cursor-pointer">
+              <img src="/delete_icon.svg" className="ml-2 h-5 w-5" />
+            </span>
+          </span>
         </div>
-      )}
+        {isOpen && (
+          <div>
+            <table className="border-t  pb-2 table-auto w-full text-center">
+              <thead className="uppercase text-xs text-slate-700 bg-slate-50">
+                <tr>
+                  <th className="p-1 bg-slate-100/50">discipline</th>
+                  <th className="bg-slate-100/50">distance</th>
+                  <th className="bg-slate-100/50">duration</th>
+                  <th colSpan={2} className="bg-slate-100/50">
+                    &nbsp;
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {workoutData &&
+                  workoutData.parts.map((part) => {
+                    return (
+                      <tr className="border-t" key={`row-${part.discipline}`}>
+                        <td className="p-2 text-xs font-semibold">
+                          {part.discipline.toUpperCase()}
+                        </td>
+                        <td className="p-4 flex justify-center align-center">
+                          <span>{part.distanceInMeters / 1000} km</span>
+                        </td>
+                        <td className="p-2">
+                          {toHours(part.durationInSeconds)}
+                        </td>
+                      </tr>
+                    );
+                  })}
+              </tbody>
+              <tfoot className="border-t bg-stone-100/50">
+                <tr>
+                  <td colSpan={5} className="px-10 py-2 text-left">
+                    <span className="text-slate-600 text-sm italic flex align-center justify-between">
+                      {editModeOn ? (
+                        <textarea
+                          defaultValue={workoutData?.notes}
+                          {...register("notes")}
+                        />
+                      ) : (
+                        <WorkoutNotes
+                          notes={workoutData?.notes || "default notes"}
+                        />
+                      )}
+                      {editModeOn && (
+                        <button className="full-w mx-auto m-4 hover:bg-blue-500 text-blue-700 hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded block uppercase tracking-wide text-xs font-bold bg-white">
+                          save changes
+                        </button>
+                      )}
+                    </span>
+                  </td>
+                </tr>
+              </tfoot>
+            </table>
+          </div>
+        )}
+      </form>
     </>
   );
 };
