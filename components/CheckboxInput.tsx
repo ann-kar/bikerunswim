7a72be9c0eaa13facc08fbb769e77d96cca77e73
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-13 21:38:09 +0200

    styles: checkbox label styles

--- components/CheckboxInput.tsx
+++ components/CheckboxInput.tsx
@@ -1,18 +1,31 @@
 interface CheckboxInputProps {
-    name: string;
-    value: string;
-    icon: string;
-    onChange: any;
-    register?: any;
-  }
+  name: string;
+  value: string;
+  icon: string;
+  onChange: any;
+  register?: any;
+}
 
-  export const CheckboxInput = ({ value, icon, onChange }: CheckboxInputProps) => {
-    return (
-      <div className="radio">
-        <label htmlFor={value} className="checkbox-input">
-          <input id={value} type="checkbox" onChange={(e:any) => onChange(e)} className="" value={value} />
-          <img className="sport-icon" src={`/${icon}`} />
-        </label>
-      </div>
-    );
-  };
+export const CheckboxInput = ({
+  value,
+  icon,
+  onChange,
+}: CheckboxInputProps) => {
+  return (
+    <div className="radio">
+      <input
+        id={value}
+        type="checkbox"
+        className="opacity-0 absolute h-8 w-8"
+        onChange={(e: any) => onChange(e)}
+        value={value}
+      />
+      <label
+        htmlFor={value}
+        className="block border-gray-100 border-[1px] rounded-lg hover:shadow-lg active:bg-blue-50 duration-200 m-4 mt-1 h-20 w-20"
+      >
+        <img className="h-20 w-20 p-2" src={`/${icon}`} />
+      </label>
+    </div>
+  );
+};
