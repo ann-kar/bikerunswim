bd498642590f3302339074347159b15bad271725
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-14 15:30:28 +0200

    refactor: move logic from page to WL comp

--- components/WorkoutList.tsx
+++ components/WorkoutList.tsx
@@ -1,7 +1,17 @@
+import { useState, useEffect } from "react";
 import { IWorkout } from "../interfaces/IWorkout";
+import { MockApi } from "../mocks/mockApi";
 import { WorkoutListItem } from "./WorkoutListItem";
 
-export const WorkoutList = ({ workouts }: { workouts: IWorkout[] }) => {
+export const WorkoutList = () => {
+  const [mockUserId, setMockUserId] = useState<string>("1");
+  const [workouts, setWorkouts] = useState<IWorkout[]>([]);
+
+  useEffect(() => {
+    const res = MockApi.getWorkouts(mockUserId);
+    res.then((res) => setWorkouts(res));
+  }, []);
+  
   return (
     <div className="w-full grow flex-wrap">
       {workouts &&
--- pages/workout-list.tsx
+++ pages/workout-list.tsx
@@ -1,24 +1,12 @@
-import { useEffect, useState } from "react";
-
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { WorkoutList } from "../components/WorkoutList";
-import { IWorkout } from "../interfaces/IWorkout";
-import { MockApi } from "../mocks/mockApi";
 
 const WorkoutListPage = () => {
-  const [mockUserId, setMockUserId] = useState<string>("1");
-  const [workouts, setWorkouts] = useState<IWorkout[]>([]);
-
-  useEffect(() => {
-    const res = MockApi.getWorkouts(mockUserId);
-    res.then((res) => setWorkouts(res));
-  }, []);
-
   return (
     <main className="flex h-[100vh] flex-col">
       <Header label="see your workouts" />
-      <WorkoutList workouts={workouts} />
+      <WorkoutList />
       <Footer link="./" label="home" />
     </main>
   );
