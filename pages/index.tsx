33f06ce123766fd7d7c43639f616090e2d277c49
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-14 15:30:45 +0200

    feat: add workout list preview

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
-import WorkoutList from "./workout-list";
+import { WorkoutList } from "../components/WorkoutList";
 
 const Home: NextPage = () => {
   return (
@@ -14,19 +14,21 @@ const Home: NextPage = () => {
       <main className="flex h-[100vh] flex-col">
         <Header label="Welcome back!" />
         <div className="w-full  grow flex-wrap bg-blue-50">
-          <div className="flex align-center h-[50%] w-full py-8 justify-center">
+          <div className="flex flex-wrap align-center h-[50%] w-full py-8 justify-center">
             <a
-              className="hover:bg-blue-500 text-blue-700 hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded block uppercase tracking-wide text-sm font-bold bg-white"
-              href="./workout-list"
-            >
-              see your recent workouts
+              className="text-blue-700 border border-blue-500 hover:text-white  hover:bg-blue-500 rounded block uppercase tracking-wide text-sm font-bold bg-white"
+              href="./workout-list">
+                <div className="pointer-events-none">
+                  <p className=" py-6 px-4">see your recent workouts</p>
+              <WorkoutList />
+                </div>
+
             </a>
           </div>
           <div className="flex align-center h-[50%] w-full py-8 justify-center">
             <a
-              className="hover:bg-blue-500 text-blue-700 hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded block uppercase tracking-wide text-sm font-bold bg-white"
-              href="./user-stats"
-            >
+              className="hover:bg-blue-500 w-6/12 text-blue-700 hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded block uppercase tracking-wide text-sm font-bold bg-white"
+              href="./user-stats">
               check out your statistics
             </a>
           </div>
