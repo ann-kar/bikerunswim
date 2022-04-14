c74c98550da6ffdfa73cac42717afe731ab3203a
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-14 22:11:46 +0200

    feat: add total workouts graph

--- pages/user-stats.tsx
+++ pages/user-stats.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
 import { StatItem } from "../components/StatItem";
+import { DoughnutChart } from "../components/stats/DoughnutChart";
 import { IStatsResponse } from "../interfaces/IStats";
 import { MockApi } from "../mocks/mockApi";
 import { mockStats } from "../mocks/mockStats";
@@ -18,13 +19,32 @@ const UserStatsPage = () => {
   return (
     <main className="flex h-[100vh] flex-col">
       <Header label="check your statistics" />
-    {stats && (
-       <div className="w-full grow flex-wrap justify-center">
-        <StatItem stats={stats.cycling} discipline="biking" />
+      {stats && (
+        <div className="px-4 py-8 grow flex-wrap">
+            <h2 className="mr-10 pr-10 leading-7 text-lg text-violet-900">
+              The discipline you practice most often is&nbsp;
+              <span className="font-bold text-violet-500">running</span>
+            </h2>
+            <div className="my-6 mx-auto flex flex-wrap justify-center">
+              <DoughnutChart
+                labels={["running", "biking", "swimming"]}
+                values={[
+                  stats.running.totalWorkouts.workoutsCount,
+                  stats.cycling.totalWorkouts.workoutsCount,
+                  stats.swimming.totalWorkouts.workoutsCount,
+                ]}
+              />
+              <small className="block w-full text-violet-900 tracking-wide text-center uppercase text-sm font-semibold">
+                Total workouts per discipline
+              </small>
+
+          </div>
+
+          {/* <StatItem stats={stats.cycling} discipline="biking" />
         <StatItem stats={stats.swimming} discipline="swimming" />
-        <StatItem stats={stats.running} discipline="running" />
-      </div>
-    )}
+        <StatItem stats={stats.running} discipline="running" /> */}
+        </div>
+      )}
       <Footer link="./" label="home" />
     </main>
   );
