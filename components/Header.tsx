9e27d8675e004d3e5da4fe9d351e1c468747fc45
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-14 11:21:43 +0200

    styles: update flexbox (grow-0)

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = ({label}: {label: string}) => {
   return (
-    <header className="align-center p-0 bg-blue-400">
+    <header className="w-full grow-0 align-center p-0 bg-blue-400">
       <div className="full-width text-md font-semibold p-4 uppercase text-white">
         {label}
       </div>
