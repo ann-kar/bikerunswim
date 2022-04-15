6d5261e1f938b9b2f175a9eb7678a153fea47c1d
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-15 09:07:10 +0200

    styles: responsive font-size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,6 +5,6 @@ interface FooterProps {
 
 export const Footer = ({label, link}: FooterProps) => {
     return (
-        <a className="text-white text-center w-full font-semibold tracking-wide grow-0 p-5 bg-blue-500 uppercase text-sm" href={link}>{label}</a>
+        <a className="text-white text-center w-full  md:p-6 md:text-xl font-semibold tracking-wide grow-0 p-5 bg-blue-400 uppercase text-sm" href={link}>{label}</a>
     )
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 export const Header = ({label}: {label: string}) => {
   return (
     <header className="w-full grow-0 align-center p-0 bg-blue-400">
-      <div className="full-width text-md font-semibold p-4 uppercase text-white">
+      <div className="full-width text-md font-semibold p-4 md:p-6 md:text-xl uppercase text-white">
         {label}
       </div>
     </header>
