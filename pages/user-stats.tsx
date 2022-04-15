1f846b40212c97f2fef609c62678c60bfc3b5367
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-15 09:45:42 +0200

    feat: select favorite sport

--- pages/user-stats.tsx
+++ pages/user-stats.tsx
@@ -8,30 +8,54 @@ import { MockApi } from "../mocks/mockApi";
 
 const UserStatsPage = () => {
   const [stats, setStats] = useState<IStatsResponse>();
+  const [favorite, setFavorite] = useState<string>();
 
   useEffect(() => {
     const res = MockApi.getStats("1");
     res.then((res) => setStats(res));
   }, []);
 
+  useEffect(() => {
+    if (stats) {
+      if (
+        stats.running.totalWorkouts.workoutsCount >
+          stats.cycling.totalWorkouts.workoutsCount &&
+        stats.running.totalWorkouts.workoutsCount >
+          stats.swimming.totalWorkouts.workoutsCount
+      ) {
+        setFavorite("running");
+      } else if (
+        stats.cycling.totalWorkouts.workoutsCount >
+        stats.swimming.totalWorkouts.workoutsCount
+      ) {
+        setFavorite("cycling");
+      } else {
+        setFavorite("swimming");
+      }
+    }
+  }, [stats]);
+
   return (
     <main className="flex h-[100vh] flex-col">
       <Header label="check your statistics" />
       {stats && (
         <div className="px-4 py-8 grow flex-wrap">
-           <h2 className="ml-10 pb-10 text-right leading-7 text-lg text-violet-900">
-          You have completed
-          <span className="font-bold text-violet-500">
-          &nbsp;{stats.running.totalWorkouts.workoutsCount +
-              stats.cycling.totalWorkouts.workoutsCount +
-              stats.swimming.totalWorkouts.workoutsCount}&nbsp;
-          </span>
-         workouts</h2>
+          <h2 className="ml-10 pb-10 text-right leading-7 text-lg text-violet-900">
+            You have completed
+            <span className="font-bold text-violet-500">
+              &nbsp;
+              {stats.running.totalWorkouts.workoutsCount +
+                stats.cycling.totalWorkouts.workoutsCount +
+                stats.swimming.totalWorkouts.workoutsCount}
+              &nbsp;
+            </span>
+            workouts
+          </h2>
           <h2 className="mr-10 pr-10 leading-7 text-lg text-violet-900">
             The discipline you practiced most often is&nbsp;
-            <span className="font-bold text-violet-500">running</span>
+            <span className="font-bold text-violet-500">{favorite}</span>
           </h2>
-          <div className="my-6 mx-auto flex flex-wrap justify-center">
+          <div className="mt-6 mx-auto flex flex-wrap justify-center">
             <DoughnutChart
               labels={["running", "cycling", "swimming"]}
               values={[
@@ -40,7 +64,7 @@ const UserStatsPage = () => {
                 stats.swimming.totalWorkouts.workoutsCount,
               ]}
             />
-            <small className="block w-full text-violet-900 tracking-wide text-center uppercase text-sm font-semibold">
+            <small className="block w-full text-violet-900 tracking-wide text-center uppercase text-md font-semibold">
               Total workouts per discipline
             </small>
           </div>
