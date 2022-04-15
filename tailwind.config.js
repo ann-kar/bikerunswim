89e9ecb800400674f836a8f557cbac07c0f58c7c
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-15 11:22:55 +0200

    feat: add screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "mobile-sm": "300px",
+        "mobile-md": "450px",
+        "mobile-lg": "520px",
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
-}
+};
