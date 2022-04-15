2d417e52f21a68069a78792c39f6d24576d40b3d
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-15 09:22:28 +0200

    styles: font-size & padding, move icons to details

--- components/WorkoutListItem.tsx
+++ components/WorkoutListItem.tsx
@@ -17,7 +17,8 @@ export const WorkoutListItem = ({ workout }: { workout: IWorkout }) => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = async (data: any) => { // TODO: refactor
+  const onSubmit = async (data: any) => {
+    // TODO: refactor
     setEditModeOn(false);
     console.log(data);
     const tempWorkoutData = workoutData;
@@ -70,9 +71,8 @@ export const WorkoutListItem = ({ workout }: { workout: IWorkout }) => {
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input type="hidden"></input>
-        <div className="bg-blue-50/70 border-t border-slate-300/40 p-3 text-sm font-semibold py-4 flex justify-between">
-          <span>
+        <div className="border-y pl-2 border-violet-200 text-md flex justify-between">
+          <span className="block mt-4">
             {editModeOn ? (
               <input
                 type="date"
@@ -80,17 +80,18 @@ export const WorkoutListItem = ({ workout }: { workout: IWorkout }) => {
                 {...register("date")}
               />
             ) : (
-              <span className="px-2">{workoutData?.date}</span>
+              <span className="block font-semibold px-4 pb-2 text-lg">
+                {workoutData?.date}
+              </span>
             )}
-            <span>
+            <span className="px-2 block mb-4">
               {workoutData?.parts.map((part, i) => {
                 return (
                   <span key={`part-${part.discipline}`}>
                     {i % 2 === 1 && <span>&ndash;</span>}
                     <span
                       key={part.discipline}
-                      className="uppercase text-sm px-2 tracking-wide"
-                    >
+                      className="uppercase px-2 tracking-wide">
                       {part.discipline}
                     </span>
                   </span>
@@ -98,27 +99,20 @@ export const WorkoutListItem = ({ workout }: { workout: IWorkout }) => {
               })}
             </span>
           </span>
-          <span className="flex">
+          <div className="flex">
             <button
               onClick={(e) => handleClick(e)}
-              className="text-sm hover:font-semibold"
-            >
-              details
+              className="text-sm mx-4 text-violet-700 hover:font-semibold">
+              see details
             </button>
-            <span onClick={updateWorkout} className="cursor-pointer">
-              <img src="/edit_icon.svg" className="ml-2 h-5 w-5" />
-            </span>
-            <span onClick={deleteWorkout} className="cursor-pointer">
-              <img src="/delete_icon.svg" className="ml-2 h-5 w-5" />
-            </span>
-          </span>
+          </div>
         </div>
         {isOpen && (
           <div>
-            <table className="border-t  pb-2 w-full table-auto  text-center">
+            <table className="border-t pb-2 w-full table-auto  text-center">
               <thead className="uppercase text-xs text-slate-700 bg-slate-50">
                 <tr>
-                  <th className="p-1 bg-slate-100/50">discipline</th>
+                  <th className="p-2 bg-slate-100/50">discipline</th>
                   <th className="bg-slate-100/50">distance</th>
                   <th className="bg-slate-100/50">duration</th>
                   <th colSpan={2} className="bg-slate-100/50">
@@ -149,7 +143,7 @@ export const WorkoutListItem = ({ workout }: { workout: IWorkout }) => {
                                 })}
                               />
                             ) : (
-                              <span>{part.distanceInMeters / 1000} km</span>
+                              <span className="block py-3">{part.distanceInMeters / 1000} km</span>
                             )}
                           </td>
                           <td className="p-2 w-1/3">
@@ -160,12 +154,13 @@ export const WorkoutListItem = ({ workout }: { workout: IWorkout }) => {
                     );
                   })}
               </tbody>
-              <tfoot className="border-t bg-stone-100/50">
+              <tfoot className="border-t bg-gray-100">
                 <tr>
-                  <td colSpan={3} className="px-10 py-2 text-left">
+                  <td colSpan={2} className=" px-10 py-2 text-left">
                     <span className="text-slate-600 text-sm italic flex align-center justify-between">
                       {editModeOn ? (
                         <textarea
+                        className="overflow-scroll"
                           defaultValue={workoutData?.notes}
                           {...register("notes")}
                         />
@@ -174,12 +169,27 @@ export const WorkoutListItem = ({ workout }: { workout: IWorkout }) => {
                           notes={workoutData?.notes || "default notes"}
                         />
                       )}
-                      {editModeOn && (
-                        <button className="mx-auto m-4 hover:bg-blue-500 text-blue-700 hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded block uppercase tracking-wide text-xs font-bold bg-white">
-                          save changes
-                        </button>
-                      )}
                     </span>
+                  </td>
+                  <td className="flex justify-end">
+                    {editModeOn ? (
+                      <button className="mx-auto m-4 hover:bg-blue-500 text-blue-700 hover:text-white py-1 px-2 border border-blue-500 hover:border-transparent rounded block uppercase tracking-wide text-xs font-bold bg-white">
+                        save changes
+                      </button>
+                    ) : (
+                      <div className="flex m-0 p-0">
+                        <img
+                          src="/edit_icon.svg"
+                          className="cursor-pointer mx-3 h-6 w-6 my-auto"
+                          onClick={updateWorkout}
+                        />
+                        <img
+                          src="/delete_icon.svg"
+                          className="cursor-pointer mx-3 h-6 w-6 my-3"
+                          onClick={deleteWorkout}
+                        />
+                      </div>
+                    )}
                   </td>
                 </tr>
               </tfoot>
