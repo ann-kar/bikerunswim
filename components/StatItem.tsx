7969b8aa9c8c86556e53a7a55557f11d9f886bc2
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-15 11:21:56 +0200

    styles: update layout

--- components/StatItem.tsx
+++ components/StatItem.tsx
@@ -1,15 +1,19 @@
-export const StatItem = ({ discipline, stats }: any) => {
+import { IStatsResponse } from "../interfaces/IStats";
+
+export const StatItem = ({ discipline, stats }: {discipline: string; stats: any}) => {
   return (
-    <div className="w-full grow flex-wrap justify-center">
-      <img src={`/${discipline}.svg`} className="m-8 w-20 h-20" />
-      <h2 className="text-xl m-8">
-        The longest distance in a single workout:
-        <span className="font-bold ml-2">{stats.topWorkout.distance/1000} km</span>
-      </h2>
-      <h2 className="text-xl m-8">
-        The highest speed:
-        <span className="font-bold ml-2">{stats.topWorkout.speed} km/h</span>
-      </h2>
+    <div className="w-full grow max-w-md mx-auto flex-wrap justify-center">
+      <div className="border rounded-xl border-violet-100 py-3 flex mt-8 align-center bg-violet-100/50">
+        <img src={`/${discipline}.svg`} className="m-8 w-16 h-16" />
+        <div>
+          <h2 className="mx-4 my-8 text-right text-lg text-violet-900">
+            Total distance you covered {discipline}:&nbsp;
+            <span className="font-bold text-violet-500">
+              {stats.totalWorkouts.distance / 1000}&nbsp;km
+            </span>
+          </h2>
+        </div>
+      </div>
     </div>
   );
 };
