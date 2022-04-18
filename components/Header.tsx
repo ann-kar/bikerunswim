079d99dd33d82acc6713236e6e99bd68da5ca763
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-18 17:26:20 +0200

    test: add data-testid

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = ({label}: {label: string}) => {
   return (
-    <header className="w-full grow-0 align-center p-0 bg-blue-400">
+    <header data-testid="header"  className="w-full grow-0 align-center p-0 bg-blue-400">
       <div className="full-width text-md font-semibold p-4 md:p-6 md:text-xl uppercase text-white">
         {label}
       </div>
