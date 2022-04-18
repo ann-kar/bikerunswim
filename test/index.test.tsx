4037c91290feaca1d67d9b04477b1f2c31cd522b
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-18 17:27:11 +0200

    test: add sample tests

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { Header } from "../components/Header";
+import Home from "../pages/index";
 
-import Home from '../pages/index';
-
-describe(Home.name, () => {
-  test('renders Home component', () => {
-    
-    render(<Home />);
-
+describe("test the testing library", () => {
+  it("renders Header with appropriate text", () => {
+    const context = render(<Header label={"hello"} />);
+    const element = context.getByTestId("header");
+    expect(element?.textContent).toBe("hello");
   });
+
+  it("renders index page", () => {
+    const context = render(<Home />);
+    const element = context.getByTestId("header");
+    expect(element?.textContent).toBe("Welcome back!");
+  });
+});
+describe("test the index page", () => {
+ 
 });
